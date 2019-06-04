3038bdd325672ff381bf0d2ffe18b74bbe2d5efc
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-04 19:08:12 +1200

    Updated Hero Banner to make it more mobile respons

--- src/views/home/heroBanner/HeroBanner.js
+++ src/views/home/heroBanner/HeroBanner.js
@@ -4,8 +4,10 @@ import './HeroBanner.scss';
 const HeroBanner = () => {
   return (
     <div className="HeroBanner row center-xs">
+      <div className="content">
         <h1 className="name">Kaiz Hudda</h1>
         <h3 className="title">Front-End Developer</h3>
+      </div>
     </div>
   );
 }
