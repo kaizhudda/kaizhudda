d8486e8012ee55c208c8b794b8b60bbc71b4b95b
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-05 18:01:15 +1200

    Added Particle effect on HeroBanner!

--- src/views/home/heroBanner/HeroBanner.js
+++ src/views/home/heroBanner/HeroBanner.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+
 import './HeroBanner.scss';
 
 const HeroBanner = () => {
   return (
     <div className="HeroBanner row center-xs">
+      <Particles
+        className="particles"
+        params={{
+          "particles": {
+            "number": {
+              "value": 75
+            },
+            "size": {
+              "value": 4
+            }
+          },
+          "interactivity": {
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              }
+            }
+          }
+        }} />
       <div className="content">
         <h1 className="name">Kaiz Hudda</h1>
         <h3 className="title">Front-End Developer</h3>
