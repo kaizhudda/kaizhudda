1a7bddde5afda799a9cc08fe35505ee1abd8a07d
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-09 19:13:21 +1200

    Added Contenful API, plus .env to hide contenful access code

--- src/views/home/summary/Summary.js
+++ src/views/home/summary/Summary.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as contentful from 'contentful';
 import Title from '../../../sharedComponents/title';
 import Subtitle from '../../../sharedComponents/subtitle';
 
 import './Summary.scss';
 
-const Summary = () => {
-  return (
-    <div className="Summary row center-xs">
-      <Title>Profile</Title>
-      <Subtitle>I build things in the pixel aether</Subtitle>
-      <div className="profile-content col-xl-5 col-lg-6 col-md-9 col-sm-11 col-xs-12">
-        <div className="picture" />
-        <p className="description">
-          Hi, I'm Kaiz Hudda, a versatile Front-End Developer with 3+ years of experience leveraging React, Redux and
-          JavaScript to build commercial web-based applications.
-          Focused on engineering user centric solutions for achieving greater customer satisfaction.
-          Thrive in working in a collaborative agile environment to deliver high quality products.
-          <br /> <br />
-          <i className="fa fa-map-marker-alt" /> <span className="location">Auckland, New Zealand</span>
-        </p>
+
+class Summary extends Component {
+  state = {
+    posts: []
+  }
+
+  client = contentful.createClient({
+    space: 'yzc3im4v4u3u',
+    accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+  })
+
+  componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
+  }
+
+  fetchPosts = () => this.client.getEntries({ 'content_type': 'summary' })
+
+  setPosts = response => {
+    this.setState({
+      posts: response.items
+    })
+  }
+
+  render() {
+    return (
+      <div className="Summary row center-xs">
+        <Title>Profile</Title>
+        <Subtitle>I build things in the pixel aether</Subtitle>
+        <div className="profile-content col-xl-5 col-lg-6 col-md-9 col-sm-11 col-xs-12">
+          <div className="picture" />
+          <p className="description">
+            {
+              this.state.posts[0] && 
+              this.state.posts[0].fields.description
+            }
+            <br /> <br />
+            <i className="fa fa-map-marker-alt" /> <span className="location">Auckland, New Zealand</span>
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Summary;
