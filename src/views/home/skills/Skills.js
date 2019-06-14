948978ec1af8e9505317726a218863fc249b4dd5
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-15 10:46:29 +1200

    Made a Skill component to enable code reuse

--- src/views/home/skills/Skills.js
+++ src/views/home/skills/Skills.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Title from '../../../sharedComponents/title';
 import Subtitle from '../../../sharedComponents/subtitle';
-import SecondaryTitle from '../../../sharedComponents/secondary-title'
+import Skill from './skill';
 
 import './Skills.scss';
 
@@ -9,100 +9,85 @@ const Skills = () => {
   return (
     <div className="Skills row center-xs">
       <Title>Skills</Title>
-      <Subtitle></Subtitle>
-      <div className="skills-content col-xs-12">
-        <SecondaryTitle>Front-End Skills</SecondaryTitle>
-        <div className="skill">
-          <div className="skill-title">HTML5 (10+ years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">CSS3 (10+ years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">Javascript ES6 (8 years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star-half-alt" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">React.js (3 years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star-half-alt" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">Redux (3 years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="far fa-star" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">SASS (3 years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="far fa-star" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">Node.js (3 years)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="far fa-star" />
-          </div>
-        </div>
-
-        <div className="skill">
-          <div className="skill-title">Styled Components (1 year)</div>
-          <div className="skill-score">
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="fas fa-star" />
-            <i className="far fa-star" />
-            <i className="far fa-star" />
-          </div>
-        </div>
-
-      </div>
+      <Subtitle>Still trying to think of a Subtitle here</Subtitle>
+      <Skill skills={feSkills} category="Front-End" />
+      <Skill skills={beSkills} category="Back-End" />
+      <Skill skills={otherSkills} category="CI/CD and other" />
     </div>
   );
 }
 
 export default Skills;
+
+const feSkills = [
+  {
+    name: "React",
+    experience: "3+ years"
+  },
+  {
+    name: "Redux",
+    experience: "3+ years"
+  }, 
+  {
+    name: "JavaScript/ES6",
+    experience: "6+ years"
+  }, 
+  {
+    name: "HTML5",
+    experience: "10+ years"
+  }, 
+  {
+    name: "CSS3",
+    experience: "8+ years"
+  }, 
+  {
+    name: "SASS",
+    experience: "3+ years"
+  },
+  {
+    name: "Jest/Enzyme",
+    experience: "2+ years"
+  }
+];
+
+const beSkills = [
+  {
+    name: "Node.js",
+    experience: "3+ years"
+  },
+  {
+    name: "C#/.NET",
+    experience: "2+ years"
+  }, 
+  {
+    name: "SQL Server",
+    experience: "2+ years"
+  }, 
+  {
+    name: "MongoDB",
+    experience: "1+ years"
+  }, 
+  {
+    name: "Express.js",
+    experience: "1+ years"
+  }
+];
+
+const otherSkills = [
+  {
+    name: "Microsoft Azure",
+    experience: "2+ years"
+  },
+  {
+    name: "Docker",
+    experience: "1+ years"
+  }, 
+  {
+    name: "Jenkins",
+    experience: "1 year"
+  },
+  {
+    name: "Git",
+    experience: "6+ years"
+  }
+];
