38dbb670394e79bc2f4111d3673f1f44e1feb6c8
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-16 23:13:05 +1200

    Added React Helmet for SEO purposes

--- src/root/App.js
+++ src/root/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Routes from '../Router';
 import Header from './header';
 
@@ -7,6 +8,10 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App row center-xs">
+      <Helmet>
+        <title>Kaiz Hudda - Front End React Developer - Digital Resume</title>
+        <meta name="description" content="Hi, I'm Kaiz Hudda, a versatile Front-End React Developer with 3+ years of experience leveraging React, Redux and JavaScript to build commercial web-based applications. Focused on engineering user centric solutions for achieving greater customer satisfaction. Thrive in working in a collaborative agile environment to deliver high quality products." />
+      </Helmet>
       <div className="col-xs-12">
         <Header />
         <Routes />
