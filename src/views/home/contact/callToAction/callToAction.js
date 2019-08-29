d79a88663ed5d882ede1f35d519775c0760c7ab0
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-08-29 20:11:33 +1200

    removed cols

--- src/views/home/contact/callToAction/callToAction.js
+++ src/views/home/contact/callToAction/callToAction.js
@@ -7,15 +7,12 @@ const CallToAction = () => {
     <section className="CallToAction center-xs">
       <div className="CallToAction__box">
         <div className="CallToAction__content">
-          <div className="col-md-3 col-xs-12">
+
             <h1 className="title">Start a project</h1>
-          </div>
-          <div className="col-md-5 col-xs-12">
+
             <p className="text">Interested in working together? Lets meet up for a chat. The <strong>coffee</strong> is on me.</p>
-          </div>
-          <div className="col-md-3 col-xs-12">
+
             <a href="mailto:kaiz.hudda@gmail.com" class="btn btn--outline">Lets do this</a>
-          </div>
         </div>
       </div>
     </section>
