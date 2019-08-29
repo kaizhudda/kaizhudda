19c8be5e3f5d8c7e5567e320a302288cbb953196
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-08-29 20:01:34 +1200

    Effects and clean up of cTa component

--- src/views/home/contact/callToAction/callToAction.js
+++ src/views/home/contact/callToAction/callToAction.js
@@ -11,10 +11,10 @@ const CallToAction = () => {
             <h1 className="title">Start a project</h1>
           </div>
           <div className="col-md-5 col-xs-12">
-            <p className="text">Interested in working together? We should queue up a chat. I’ll buy the coffee.</p>
+            <p className="text">Interested in working together? Lets meet up for a chat. The <strong>coffee</strong> is on me.</p>
           </div>
           <div className="col-md-3 col-xs-12">
-            <a href="#" class="btn btn--outline">Lets do this</a>
+            <a href="mailto:kaiz.hudda@gmail.com" class="btn btn--outline">Lets do this</a>
           </div>
         </div>
       </div>
